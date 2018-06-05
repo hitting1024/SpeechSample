9e2c74b696d0f64fdc94a823722fc4c6f14e8b37
IOKA <hit0apps@gmail.com>
2018-06-06 00:03:01 +0900

    authorize speech

--- SpeechSample/ViewController.swift
+++ SpeechSample/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Speech
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
+    private let audioEngine = AVAudioEngine()
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        SFSpeechRecognizer.requestAuthorization({ authStatus in
+            // for main thread
+            OperationQueue.main.addOperation({ [weak self] in
+                guard let `self` = self else { return }
+                switch authStatus {
+                case .authorized:
+                    self.button.isEnabled = true
+                case .denied:
+                    self.button.isEnabled = false
+                    self.button.setTitle("Access Denied", for: .disabled)
+                case .restricted:
+                    self.button.isEnabled = false
+                    self.button.setTitle("Not supported", for: .disabled)
+                case .notDetermined:
+                    self.button.isEnabled = false
+                    self.button.setTitle("Not Allowed", for: .disabled)
+                }
+            })
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.speechRecognizer.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension ViewController: SFSpeechRecognizerDelegate {
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            self.button.isEnabled = true
+            self.button.setTitle("Start Speech", for: [])
+        } else {
+            self.button.isEnabled = false
+            self.button.setTitle("Stop Speech", for: .disabled)
+        }
+    }
+    
+}
