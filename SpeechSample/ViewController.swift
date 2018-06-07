bd3bfa83f849b9e3ea269dc18a09fa1902330f45
IOKA <hit0apps@gmail.com>
2018-06-07 23:55:24 +0900

    speech recognition

--- SpeechSample/ViewController.swift
+++ SpeechSample/ViewController.swift
@@ -56,6 +56,80 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    private func startRecording() throws {
+        self.refreshTask()
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(AVAudioSessionCategoryRecord)
+        try audioSession.setMode(AVAudioSessionModeMeasurement)
+        try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        
+        self.recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = self.audioEngine.inputNode
+        guard let recognitionRequest = self.recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        self.recognitionTask = self.speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { [weak self] result, error in
+            guard let `self` = self else { return }
+            
+            var isFinal = false
+            
+            if let result = result {
+                self.label.text = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.button.isEnabled = true
+                self.button.setTitle("Start Speech", for: [])
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat, block: { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+        })
+        
+        try startAudioEngine()
+    }
+    
+    private func refreshTask() {
+        if let recognitionTask = self.recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+    }
+    
+    private func startAudioEngine() throws {
+        self.audioEngine.prepare()
+        try self.audioEngine.start()
+        label.text = "Please Speech..."
+    }
+    
+}
+
+extension ViewController {
+    
+    @IBAction func startSpeech(_ sender: UIButton) {
+        if self.audioEngine.isRunning {
+            self.audioEngine.stop()
+            self.recognitionRequest?.endAudio()
+            self.button.isEnabled = false
+            self.button.setTitle("Stopping...", for: .disabled)
+        } else {
+            try! self.startRecording()
+            self.button.setTitle("Stop Speech", for: [])
+        }
+    }
+    
 }
 
 extension ViewController: SFSpeechRecognizerDelegate {
